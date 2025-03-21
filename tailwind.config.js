8138e46c49b03423f2716231adf431db60873862
sophialiang <liang.yime@northeastern.com>
2025-03-20 23:58:56 -0700

    install react js and tailwind css and create the first template

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // 确保 Tailwind 可以扫描你的 HTML/JSX
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
