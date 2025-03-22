098d6a8bd076a4c28fb26680c849bd8d5103b1da
sophialiang <liang.yime@northeastern.com>
2025-03-21 23:57:05 -0700

    complete sign up page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -11,7 +11,7 @@ export const signup = async (req, res, next) => {
     res.status(201).json("User created successfully!")
 
   } catch(error) {
-    next(errorHandler(550, 'error from the function'));
+    next(error);
   }
 
 };
