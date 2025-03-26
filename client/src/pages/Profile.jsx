806d66ae744554e4d8f0a5cf8156424c99c3d361
sophialiang <liang.yime@northeastern.com>
2025-03-25 23:00:34 -0700

    Fixed profile picture not showing on profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 
 export default function Profile() {
-  const currentUser = useSelector((state) => state.user);
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
